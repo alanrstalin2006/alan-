a23348ad894c9088af679aecd2717f95f8bd9df2
Alan r stalin <stalinrini344@gmail.com>
2025-12-15 09:29:18 +0530

    Add Pascal's Triangle and sum programs
    Added alan.c, which prints Pascal's Triangle for a user-specified number of rows, and alan.cpp, which sums two user-input numbers. Also added the compiled binary alan.exe.

---
+++ alan.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{ 
+    int a,b,sum=0;
+    printf("enter two numbers: ");
+    scanf("%d%d",&a,&b);
+    sum = a + b;
+    printf("sum of number is: %d",sum);
+    getch();
+    return 0;   
+    
+}  
